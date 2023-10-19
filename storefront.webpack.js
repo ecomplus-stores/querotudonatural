42b581ec8bc4256595f5a51594675a0ced097481
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-10-19 02:03:45 -0300

    Update storefront.webpack.js

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -4,9 +4,10 @@ module.exports = () => ({
   resolve: {
     alias: {
 /*         './html/AddressForm.html': path.resolve(__dirname, 'template/js/html/AddressForm.html'),
-        './js/AddressForm.js': path.resolve(__dirname, 'template/js/js/AddressForm.js'), */
+        './js/AddressForm.js': path.resolve(__dirname, 'template/js/js/AddressForm.js'), 
         './html/InputZipCode.html': path.resolve(__dirname, 'template/js/html/InputZipCode.html'),
         './js/InputZipCode.js': path.resolve(__dirname, 'template/js/js/InputZipCode.js')
+        */
     }
   }
 })
