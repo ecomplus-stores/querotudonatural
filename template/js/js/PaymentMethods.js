5db0badb95194052a91d99050178c2ef86c68cbd
Vitor Rocha Gonçalves <35348281+vitorrgg@users.noreply.github.com>
2024-03-11 14:00:57 -0300

    Update PaymentMethods.js

--- template/js/js/PaymentMethods.js
+++ template/js/js/PaymentMethods.js
@@ -86,6 +86,7 @@ import {
         selectedGateway: -1,
         loadedClients: {},
         restrictIds: [
+          "65ec925e87ab487fbfdd9152",  
           "65ec925e87ab487fbfdd9152",
           "627be2da3da6d37bc22e3a8a",
     "627be2863da6d37bc22e3914",
